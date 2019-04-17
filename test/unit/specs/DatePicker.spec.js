7e94beaa5ffe700dbda6fec5e62837eb5690b1ca
Victor <vk2r@outlook.com>
2019-04-17 17:23:59 -0400

    Up test proyect

---
+++ test/unit/specs/DatePicker.spec.js
@@ -0,0 +1,17 @@
+// import { mount } from '@vue/test-utils';
+// import DatePicker from '@/components/DatePicker';
+
+describe('DatePicker', () => {
+  it('renders props.fromPage', () => {
+    // const fromPage = { month: 4, year: 2001 };
+    // const wrapper = mount(DatePicker, {
+    //   propsData: {
+    //     fromPage,
+    //     isInline: true,
+    //   },
+    // });
+    // const cal = wrapper.find('div');
+    // console.log(cal.html());
+    // expect(2).toBe(2);
+  });
+});
